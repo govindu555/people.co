578e1e11ca720258a4c4ad0e209424dd7a23e18c
govind <govind@gmail.com>
2025-07-16 12:44:26 +0530

    add sort method

--- src/secondpage/index.jsx
+++ src/secondpage/index.jsx
@@ -42,11 +42,21 @@ const SecondPage=()=>{
         nav(`/edit/${id}`)
      }
 
+     function sortfun(){
+        let d=[...data]
+        if(d.length>0){
+            let result=d.sort((a,b)=>a.name.localeCompare(b.name))
+            setData(result)
+        }
+     }
+
+
      function searchfun(e){
         if(e.target.value!=""){
              const d=search.filter(i=>i.name.includes(e.target.value) || i.role.includes(e.target.value) || i.email.includes(e.target.value))
-            setData(d)
-           
+            if(d.length>0){
+                setData(d)
+            } 
         }
         else{
           setData(search)
@@ -59,7 +69,7 @@ const SecondPage=()=>{
             <div className="secondbar">
                 <h1 className="secondhead">Team members <span>{data.length}</span></h1>
                 <div className="search2"><input className="search" type="search" placeholder="Search, name,role,email" onChange={searchfun}/><BsSearchHeart className="searchicon"/></div>
-                <div className="add"><button className="addbutton" onClick={addmember}>+ ADD MEMBER</button></div>
+                <div className="add"><button className="addbutton" onClick={sortfun}>Sort name</button><button className="addbutton" onClick={addmember}>+ ADD MEMBER</button></div>
             </div>
             <div className="table">
                 <div className="tablehead">
